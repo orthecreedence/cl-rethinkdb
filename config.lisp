4dfb164dd3333c6e45b743b9a5f53b1c4c84275b
Andrew Danger Lyon <orthecreedence@gmail.com>
2013-11-29 22:50:09 -0800

    config typo fix

--- config.lisp
+++ config.lisp
@@ -4,4 +4,4 @@
   "Defines whether data returned as a sequence should be a :list or :array.")
 
 (defvar *object-type* :hash
-  "Defines whether data returned as an object should be an :aslist or :hash.")
+  "Defines whether data returned as an object should be an :alist or :hash.")
