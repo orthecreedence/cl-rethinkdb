2d38e3bb0eb7c57e4fd54a9425beff5669aa51cf
Andrew Danger Lyon <orthecreedence@gmail.com>
2014-05-23 17:37:48 -0700

    porting type system over from master (v1.12.x).

--- reql/types.lisp
+++ reql/types.lisp
@@ -21,6 +21,21 @@
 (deftype object () '(or hash-table alist))
 (deftype object-collection () '(satisfies object-collection-p))
 
+(defun is-any (object)
+  "Determine if the type is indeterminate. This can be something like a funcall,
+   a branch, a var, etc...anything whos value is only known at runtime.
+   
+   This should be extended by every type, except streams (and possibly order)."
+  (is-term (list +term-term-type-funcall+
+                 +term-term-type-var+
+                 +term-term-type-implicit-var+
+                 +term-term-type-get-field+
+                 +term-term-type-branch+
+                 +term-term-type-default+
+                 +term-term-type-coerce-to+
+                 +term-term-type-reduce+)
+           object))
+
 (defun is-select (object)
   "Determine if a given object is a selection term, which returns (possibly)
    either a StreamSelect or SingleSelect object."
@@ -36,6 +51,7 @@
   "Determine if a given object is a sequence term/object."
   (or (typep object 'list)
       (typep object 'vector)
+      (is-any object)
       (is-select object)
       (is-array object)
       (is-term (list +term-term-type-slice+
@@ -43,7 +59,6 @@
                      +term-term-type-limit+
                      +term-term-type-pluck+
                      +term-term-type-without+
-                     +term-term-type-default+
                      +term-term-type-with-fields+
                      +term-term-type-indexes-of+
                      +term-term-type-sample+
@@ -58,10 +73,7 @@
                      +term-term-type-inner-join+
                      +term-term-type-outer-join+
                      +term-term-type-eq-join+
-                     +term-term-type-zip+
-                     +term-term-type-var+
-                     +term-term-type-funcall+
-                     +term-term-type-branch+)
+                     +term-term-type-zip+)
                object)
       (and (is-term +term-term-type-datum+ object)
            (= (type (datum object)) +datum-datum-type-r-array+))))
@@ -69,12 +81,12 @@
 (defun is-array (object)
   "Determine if the given object is an array term/type."
   (or (listp object)
+      (is-any object)
       (is-term (list +term-term-type-make-array+
                      +term-term-type-index-list+
                      +term-term-type-append+
                      +term-term-type-prepend+
                      +term-term-type-difference+
-                     +term-term-type-get-field+
                      +term-term-type-set-insert+
                      +term-term-type-set-intersection+
                      +term-term-type-set-union+
@@ -85,16 +97,13 @@
                      +term-term-type-change-at+
                      +term-term-type-keys+
                      +term-term-type-db-list+
-                     +term-term-type-table-list+
-                     +term-term-type-var+
-                     +term-term-type-funcall+
-                     +term-term-type-branch+
-                     +term-term-type-coerce-to+)
+                     +term-term-type-table-list+)
                object)))
 
 (defun is-boolean (object)
   "Determine if the given object is a boolean term/type."
   (or (typep object 'boolean)
+      (is-any object)
       (is-term (list +term-term-type-eq+
                      +term-term-type-ne+
                      +term-term-type-lt+
@@ -102,34 +111,23 @@
                      +term-term-type-gt+
                      +term-term-type-ge+
                      +term-term-type-not+
-                     +term-term-type-get-field+
                      +term-term-type-contains+
                      +term-term-type-is-empty+
                      +term-term-type-has-fields+
                      +term-term-type-any+
-                     +term-term-type-all+
-                     +term-term-type-var+
-                     +term-term-type-funcall+
-                     +term-term-type-default+
-                     +term-term-type-branch+)
+                     +term-term-type-all+)
                object)))
 
 (defun is-object (object)
   "Determines if an object is an object type."
   (or (typep object 'alist)
       (typep object 'hash-table)
-      (is-function object)
+      (is-any object)
       (is-term (list +term-term-type-get+
                      +term-term-type-datum+
                      +term-term-type-index-create+
                      +term-term-type-index-drop+
-                     +term-term-type-implicit-var+
-                     +term-term-type-var+
-                     +term-term-type-get-field+
-                     +term-term-type-reduce+
                      +term-term-type-nth+
-                     +term-term-type-funcall+
-                     +term-term-type-default+
                      +term-term-type-pluck+
                      +term-term-type-without+
                      +term-term-type-merge+
@@ -141,8 +139,6 @@
                      +term-term-type-table-create+
                      +term-term-type-table-drop+
                      +term-term-type-foreach+
-                     +term-term-type-funcall+
-                     +term-term-type-branch+
                      +term-term-type-info+
                      +term-term-type-literal+)
                object)))
@@ -150,46 +146,37 @@
 (defun is-string (object)
   "Determines if an object can be used as a string term."
   (or (stringp object)
-      (is-function object)
-      (is-term (list +term-term-type-add+
-                     +term-term-type-var+
-                     +term-term-type-get-field+
-                     +term-term-type-funcall+
-                     +term-term-type-branch+)
+      (is-any object)
+      (is-term (list +term-term-type-add+)
                object)))
 
 (defun is-number (object)
   "Determines if an object can be used as a number term."
   (or (realp object)
-      (is-function object)
+      (is-any object)
       (is-term (list +term-term-type-add+
                      +term-term-type-sub+
                      +term-term-type-mul+
                      +term-term-type-div+
                      +term-term-type-mod+
-                     +term-term-type-get-field+
                      +term-term-type-count+
-                     +term-term-type-var+
-                     +term-term-type-default+
-                     +term-term-type-funcall+
-                     +term-term-type-branch+)
+                     +term-term-type-var+)
                object)))
 
 (defun is-datum (object)
   "Test if an object is a datum type."
-  (or (is-string object)
+  (or (is-any object)
+      (is-string object)
       (is-number object)
       (is-object object)
       (is-boolean object)
-      (is-term (list +term-term-type-default+
-                     +term-term-type-match+
-                     +term-term-type-var+
-                     +term-term-type-coerce-to+)
+      (is-term (list +term-term-type-match+)
                object)))
 
 (defun is-path (object)
   "Test if an object is a RethinkDB object path."
-  (or (is-string object)
+  (or (is-any object)
+      (is-string object)
       (is-object object)
       (is-array object)
       (is-datum object)))
@@ -199,17 +186,16 @@
 (defun is-pkey (object)
   "Determines if an object is a primary key term."
   (or (typep object 'pkey)
+      (is-any object)
       (is-string object)
       (is-number object)))
 
 (defun is-function (object)
   "Test if an object is a term function or reql-function object."
-  (or (is-term (list +term-term-type-func+
+  (or (is-any object)
+      (is-term (list +term-term-type-func+
                      +term-term-type-make-obj+
-                     +term-term-type-javascript+
-                     +term-term-type-funcall+
-                     +term-term-type-var+
-                     +term-term-type-branch+)
+                     +term-term-type-javascript+)
                object)))
 
 (defun is-order (object)
