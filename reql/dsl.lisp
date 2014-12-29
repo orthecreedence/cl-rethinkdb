9cc52e7702b880997cabf018f2f53b9da5d5b089
Andrew Danger Lyon <orthecreedence@gmail.com>
2014-12-29 12:43:33 -0800

    doing kind of a hybrid of what i did before. using macros now to take advantage of lexical binding/symbol macrolet crap so i dont have to reinvent it, but also doing a much simpler form of `r` that just basically prepends call to any list starting with a keyword

--- reql/dsl.lisp
+++ reql/dsl.lisp
@@ -1,15 +1,16 @@
 (in-package :cl-rethinkdb-reql)
 
 (defun query-builder (form)
-  "Takes a query form and turns it into a tree of query objects."
-  (if (and (listp form)
-           (keywordp (car form)))
-      (apply 'call
-             (car form)
-             (mapcar 'query-builder (cdr form)))
-      form))
+  "Takes a query form and turns it into a call tree that builds a query when
+   evaled."
+  (cond ((and (listp form)
+              (keywordp (car form)))
+         (cl:append (list 'call
+                          (car form))
+                    (mapcar 'query-builder (cdr form))))
+        (t form)))
 
-(defmacro r (&body query-form)
+(defmacro r (query-form)
   "Wraps query generation in a macro that takes care of pesky function naming
    issues. For instance, the function `count` is reserved in CL, so importing
    cl-rethinkdb-reql:count into your app might mess a lot of things up.
@@ -24,5 +25,5 @@
    
    This allows you to separate CL functions from query functions both logically
    and visually."
-  `(query-builder ',query-form))
+  (query-builder query-form))
 
